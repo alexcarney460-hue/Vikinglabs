832a80bdc5d8f171ae43468931d03cacfb06ea27
Alex Ablaze <gardenablaze@gmail.com>
2026-02-11 21:29:58 -0800

    chore(stripe): avoid throwing at build time when STRIPE_SECRET_KEY missing; export nullable stripe instance

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,8 +2,5 @@ import Stripe from 'stripe';
 
 const secretKey = process.env.STRIPE_SECRET_KEY;
 
-if (!secretKey) {
-  throw new Error('STRIPE_SECRET_KEY is not set');
-}
+export const stripe = secretKey ? new Stripe(secretKey) : null;
 
-export const stripe = new Stripe(secretKey);
