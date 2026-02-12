77e83f742754bfa9dc99df56816c966fa2af9e37
Alex Ablaze <gardenablaze@gmail.com>
2026-02-11 21:33:32 -0800

    chore(coinbase): export nullable resources variable instead of conditional export

--- src/lib/coinbase.ts
+++ src/lib/coinbase.ts
@@ -2,11 +2,11 @@ import CoinbaseCommerce from 'coinbase-commerce-node';
 
 const apiKey = process.env.COINBASE_COMMERCE_API_KEY;
 
+let resources: any = null;
+
 if (apiKey) {
   CoinbaseCommerce.Client.init(apiKey);
-  export const { resources } = CoinbaseCommerce;
-} else {
-  // If not configured, export null so routes can handle missing config at runtime
-  // @ts-ignore
-  export const resources = null;
+  resources = CoinbaseCommerce.resources;
 }
+
+export { resources };
